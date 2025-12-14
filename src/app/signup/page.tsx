2c64d65f7f769e413e2c6ef9bdf543e6590bf6c2
Brown <te.chnomb47@gmail.com>
2025-12-14 10:58:07 +0300

    hij

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,6 @@
 import { LoginGradient } from '@/components/gradients/login-gradient';
 import '../../styles/login.css';
 import { LoginCardGradient } from '@/components/gradients/login-card-gradient';
-import { GhLoginButton } from '@/components/authentication/gh-login-button';
 import { SignupForm } from '@/components/authentication/sign-up-form';
 
 export default function SignupPage() {
@@ -17,7 +16,6 @@ export default function SignupPage() {
           <LoginCardGradient />
           <SignupForm />
         </div>
-        <GhLoginButton label={'Sign up with GitHub'} />
         <div
           className={
             'mx-auto w-[343px] md:w-[488px] bg-background/80 backdrop-blur-[6px] px-6 md:px-16 pt-0 py-8 gap-6 flex flex-col items-center justify-center rounded-b-lg'
